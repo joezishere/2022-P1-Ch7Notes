b46e8ffc066ed8a74c6b0d72ad9db4578bcb035c
Joezishere <Joseph.zinni@vtsd.con>
2022-12-09 08:58:00 -0500

    notes

---
+++ VoidMultiParam/Example.java
@@ -0,0 +1,10 @@
+package VoidMultiParam;
+
+public class Example {
+    public static void main(String args[]){
+        }
+    public static void heyTaxi(String location, double cash) {
+        System.out.println("the taxi will take you do" + location);
+    }
+}
+
---
+++ VoidReturn.java
@@ -0,0 +1,11 @@
+
+public class VoidReturn {
+
+    public static void main(String args[]){
+            int sum = Calculator.addNums();
+            System.out.print("Your subtotal was:" + sum);
+    }
+
+
+    }
+
